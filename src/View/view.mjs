70c5fc90874b089b888884c067677bcf1ab71079
Macbook <macbook2020@Macbooks-MacBook-Pro.local>
2022-05-03 21:31:55 +0400

    final

--- src/View/view.mjs
+++ src/View/view.mjs
@@ -2,8 +2,8 @@ export default class View {
   constructor() {
     this.inputFrom = document.getElementById("inputFrom");
     this.inputIn = document.getElementById("inputIn");
-    this.radio1 = document.querySelectorAll("input[name='first-radio']");
-    this.radio2 = document.querySelectorAll("input[name='second-radio']");
+    this.leftRadio = document.querySelectorAll("input[name='first-radio']");
+    this.rightRadio = document.querySelectorAll("input[name='second-radio']");
     this.rateFrom = document.getElementById("rateFrom");
     this.rateIn = document.getElementById("rateIn");
   }
